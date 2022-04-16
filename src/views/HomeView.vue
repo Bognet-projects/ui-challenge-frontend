02d1e73a4a206d04e88e5dbcb6de646b814d3f46
Garkes <bognar.balazs@bognet.hu>
2022-04-16 13:03:38 +0200

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="home">
+
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HomeView extends Vue {}
+</script>
